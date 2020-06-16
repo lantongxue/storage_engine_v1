24633f7429471dfcb501bef95b62ff207baf7263
kali <863448246@qq.com>
2020-06-16 18:07:33 +0800

    update

--- src/Engine/COSEngine.php
+++ src/Engine/COSEngine.php
@@ -6,6 +6,7 @@ namespace V1\StorageEngine\Engine;
 
 use Qcloud\Cos\Client;
 use V1\StorageEngine\Entity\StreamBuffer;
+use V1\StorageEngine\Entity\FileInfo;
 
 class COSEngine extends BaseEngine
 {
@@ -20,6 +21,25 @@ class COSEngine extends BaseEngine
         $this->Client = new Client($this->options);
     }
 
+    protected function DownloadFile()
+    {
+        // 创建一个空白的临时文件到系统临时目录
+        $localPath = tempnam(sys_get_temp_dir(), '_SE_COS_TEMP_');
+        try {
+            $result = $this->Client->getObject([
+                'Bucket' => $this->Bucket,
+                'Key' => $this->FileInfo->FullName,
+                'SaveAs' => $localPath
+            ]);
+            print_r($result);
+        }
+        catch (\Exception $exception)
+        {
+            unlink($localPath); // 删除创建的空白临时文件
+            throw $exception;
+        }
+    }
+
     public function ReadAsText(): string
     {
 
@@ -27,7 +47,8 @@ class COSEngine extends BaseEngine
 
     public function ReadAsStreamBuffer(): StreamBuffer
     {
-
+        $this->DownloadFile();
+        return StreamBuffer::FromFile(new FileInfo('22'));
     }
 
     public function WriteText(string $content): int
